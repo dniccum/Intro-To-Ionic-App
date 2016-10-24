6c6324c6c7979fd5f6bd08a434f617720da7145f
Doug Niccum <dniccumdesign@gmail.com>
2016-10-23 20:50:46 -0500

    Imports the settings page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController, ModalController } from 'ionic-angular';
 import { ModalForm } from '../../components/modal-form/modal-form';
+import { Settings } from '../settings/settings';
 
 @Component({
   selector: 'page-home',
@@ -15,7 +16,14 @@ export class HomePage {
 
   showAddModal() {
     let modalForm = this.modalController.create(ModalForm);
+    modalForm.onDidDismiss(data => {
+      // do something
+    });
     modalForm.present();
+  }
+
+  viewSettings() {
+    this.navCtrl.push(Settings);
   }
 
 }
