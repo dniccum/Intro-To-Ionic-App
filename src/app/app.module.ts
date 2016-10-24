f503f2a8a7d673ebc023162d9ae8b4e3f552e0ae
Doug Niccum <dniccumdesign@gmail.com>
2016-10-23 20:50:10 -0500

    Adds the settings and app info pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Settings } from '../pages/settings/settings';
+import { AppInfo } from '../pages/app-info/app-info';
 import { ModalForm } from '../components/modal-form/modal-form'
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ModalForm
+    ModalForm,
+    Settings,
+    AppInfo
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +21,9 @@ import { ModalForm } from '../components/modal-form/modal-form'
   entryComponents: [
     MyApp,
     HomePage,
-    ModalForm
+    ModalForm,
+    Settings,
+    AppInfo
   ],
   providers: []
 })
