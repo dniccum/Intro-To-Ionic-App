497c635d68171bd9365691ed53d036454d9a470a
Doug Niccum <dniccumdesign@gmail.com>
2016-10-23 20:49:41 -0500

    Initializes the SQLite plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { StatusBar, SQLite } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 
@@ -16,6 +16,20 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+
+      let db = new SQLite();
+      db.openDatabase({
+        name: 'data.db',
+        location: 'default'
+      }).then(() => {
+        db.executeSql('CREATE TABLE IF NOT EXISTS DataTable (firstName VARCHAR(32), lastName VARCHAR(32), whoPaid  VARCHAR(4))', {}).then(() => {
+          
+        }, (err) => {
+          console.error('Unable to execute sql: ', err);
+        });
+      }, (err) => {
+        console.error('Unable to open database: ', err);
+      })
     });
   }
 }
