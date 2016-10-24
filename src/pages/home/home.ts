b1fd61689d01fd15e38692181cacc2971b7ca597
Doug Niccum <dniccumdesign@gmail.com>
2016-10-24 00:15:09 -0500

    Finally updates the function for the home component.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController, Platform, ActionSheetController, ToastController } from 'ionic-angular';
+import { SQLite } from 'ionic-native';
 import { ModalForm } from '../../components/modal-form/modal-form';
 import { Settings } from '../settings/settings';
 
@@ -10,20 +11,120 @@ import { Settings } from '../settings/settings';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public modalController: ModalController ) {
-    
+  public resultArray: Object[] = [];
+  constructor(public navCtrl: NavController, public modalController: ModalController, platform: Platform, public actionSheetCtrl: ActionSheetController, public toastController: ToastController) {
+    platform.ready().then(() => {
+      this.getResults();
+    });
   }
 
   showAddModal() {
     let modalForm = this.modalController.create(ModalForm);
     modalForm.onDidDismiss(data => {
-      // do something
+      this.getResults();
     });
     modalForm.present();
   }
 
   viewSettings() {
     this.navCtrl.push(Settings);
+  }
+
+  getResults() {
+    let db = new SQLite();
+
+    // reset
+    this.resultArray.length = 0;
+    
+    db.openDatabase({
+      name: 'data.db',
+      location: 'default'
+    }).then(() => {
+      db.executeSql('SELECT id, firstName, lastName, whoPaid FROM DataTable ORDER BY firstName ASC', {}).then((results) => {
+        if (results["rows"].length > 0) {
+          for(let i = 0; i < results["rows"].length; i++) {
+            console.log(JSON.stringify(results["rows"].item(i)));
+
+            let dbRow: Object = {
+              id: results["rows"].item(i).id,
+              firstName: results["rows"].item(i).firstName,
+              lastName: results["rows"].item(i).lastName,
+              whoPaid: results["rows"].item(i).whoPaid
+            };
+            this.resultArray.push(dbRow);
+          }
+        }
+      }, (err) => {
+        console.error('Unable to execute sql: ', JSON.stringify(err));
+      });
+    });
+  }
+
+  deleteResult(index, id) {
+    let db = new SQLite();
+    let toast = this.toastController.create({
+        message: `${this.resultArray[index]["firstName"]} ${this.resultArray[index]["lastName"]} was successfully removed.`,
+        position: 'top',
+        cssClass: 'success',
+        duration: 2000
+      });
+    
+    db.openDatabase({
+      name: 'data.db',
+      location: 'default'
+    }).then(() => {
+      db.executeSql('DELETE FROM DataTable WHERE id = ?', [id]).then(() => {
+        toast.present();
+        this.resultArray.splice(index, 1);
+      }, (err) => {
+        console.error('Unable to execute sql: ', JSON.stringify(err));
+      });
+    });
+  }
+
+  showDelete(index: number, id: number) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: `Delete ${this.resultArray[index]["firstName"]} ${this.resultArray[index]["lastName"]}?`,
+      buttons: [
+        {
+          text: 'Yep, delete them.',
+          role: 'destructive',
+          handler: () => {
+            this.deleteResult(index, id);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {}
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  toggleResult(index: number, id: number, whoPaid: string) {
+    let db = new SQLite();
+
+    console.log(id, whoPaid);
+
+    // update model
+    if (whoPaid === 'you') {
+      this.resultArray[index]["whoPaid"] = 'them';
+    } else {
+      this.resultArray[index]["whoPaid"] = 'you';
+    }
+
+    // update database
+    db.openDatabase({
+      name: 'data.db',
+      location: 'default'
+    }).then(() => {
+      db.executeSql('UPDATE DataTable SET whoPaid = ? WHERE id = ?', [whoPaid, id]).then((results) => {
+
+      }, (err) => {
+        console.error('Unable to execute sql: ', JSON.stringify(err));
+      });
+    });
   }
 
 }
